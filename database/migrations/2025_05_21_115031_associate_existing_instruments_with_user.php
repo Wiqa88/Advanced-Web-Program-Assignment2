598614a8f2fb1e9536bb75a14f810a62092cbe05
Wiqa88 <wiqa88@gmail.com>
2025-05-22 21:14:49 +0100

    done more

--- database/migrations/2025_05_21_115031_associate_existing_instruments_with_user.php
+++ database/migrations/2025_05_21_115031_associate_existing_instruments_with_user.php
@@ -15,6 +15,7 @@ return new class extends Migration
             ['email' => 'test@example.com'],
             [
                 'name' => 'Test User',
+                'password' => bcrypt('password'),
             ]
         );
 
